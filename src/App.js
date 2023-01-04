7d63731fd112b5eada71d9043e68ed41f09b7e49
Wes <averagewizard13@gmail.com>
2023-01-03 20:24:54 -0800

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{opacity: 1}}>S-DES Gamestate Manipulator</h1>
+      <div style={{display: "flex", minHeight: "1000px"}}>
+        <p>Here is some dummy text that needs to take up space on the page. It is intentionally long.</p>
+        <hr />
+        <p>Here is some dummy text that needs to take up space on the page. It is intentionally long.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
